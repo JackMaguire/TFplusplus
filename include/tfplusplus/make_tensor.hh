788999b26cfd704fa777978d8f661a437624be01
Jack Maguire <jackmaguire1444@gmail.com>
2021-04-07 15:57:11 -0400

    first draft with floats

--- include/tfplusplus/make_tensor.hh
+++ include/tfplusplus/make_tensor.hh
@@ -73,7 +73,7 @@ get_num_dims(){
   }
 }
 
-template< typename T, int NDIM >
+template< int NDIM, typename T >
 void
 get_dims(
   T const & t,
@@ -93,8 +93,16 @@ get_dims(
     assert( ! t.empty() );
     dims[ current_dim ] = t.size();
   
-    get_dims< typename T::value_type, NDIM >( t[0], dims, current_dim + 1 );
+    get_dims< NDIM, typename T::value_type >( t[0], dims, current_dim + 1 );
   }
+}
+
+template< int NDIM, typename T >
+std::array< int, NDIM >
+get_dims( T const & t ) {
+  std::array< int, NDIM > dims;
+  get_dims< NDIM >( t, dims, 0 );
+  return dims;
 }
 
 template< typename T >
@@ -110,12 +118,24 @@ get_first_float_ptr( T const & t ){
 }
 
 template< typename T >
+constexpr
+bool
+has_float_value_type(){
+  if constexpr( arithmetic< T > ){
+    return std::is_same_v< T, float >;
+  } else {
+    static_assert( std::ranges::range< T > );
+    return has_float_value_type< typename T::value_type >();
+  }
+}
+
+template< typename T >
 void
 get_values(
   T const & t,
   std::vector< float > & values
 ){
-  static_assert( assert_float_value_type< T >(), "assumes float" );
+  static_assert( has_float_value_type< T >(), "assumes float" );
 
   if constexpr ( arithmetic< typename T::value_type > ) {
     static_assert( false, "Dead Code?" );
@@ -133,13 +153,13 @@ get_values(
 
     if constexpr( std::is_trivially_copyable< typename T::value_type >::value ){
       //Can do copying here
-      std::cout << "CAN COPY" << std::endl;
+      //std::cout << "CAN COPY" << std::endl;
       float const * data = get_first_float_ptr( t );
       int64_t const nval_to_copy = get_num_values( t ); 
       values.insert( values.end(), data, data + nval_to_copy );
       return;
     } else {
-      std::cout << "NO COPY" << std::endl;
+      //std::cout << "NO COPY" << std::endl;
       for( auto & subrange : t ){
 	get_values< typename T::value_type >( subrange, values );
       }
@@ -149,23 +169,12 @@ get_values(
 }
 
 
-/*template< typename T >
-constexpr
-void
-assert_float_value_type(){
-  if constexpr( arithmetic< T > ){
-    static_assert( std::is_same_v< T, float >, "Must use float" );
-  } else if constexpr() {
-
-  }
-}*/
-
 //From stack overflow
-TF_Tensor *
+/*TF_Tensor *
 FloatTensor(
-  int64_t const * dims,     //DONE
-  int const num_dims,       //DONE
-  int64_t const num_values, //DONE
+  int64_t const * dims,
+  int const num_dims,
+  int64_t const num_values,
   float const * values
 ) {
   TF_Tensor *t =
@@ -174,15 +183,25 @@ FloatTensor(
   memcpy(TF_TensorData(t), values, sizeof(float) * num_values);
 
   return t;
-}
+}*/
 
 template< typename Container >
 TF_Tensor *
-run( Container const & values ){
+run( Container const & data4tensor ){
   static_assert( std::ranges::range< Container >, "Must pass a range of arithmetic" );
 
-  constexpr int ndims = get_num_dims< Container >();
-  int64_t const nvalues = get_num_values( values );
+  constexpr int NDIM = get_num_dims< Container >();
+  std::array< int, NDIM > const dims = get_dims( data4tensor );
+
+  int64_t const nvalues = get_num_values( data4tensor );
+  std::vector< float > values;
+  get_values( data4tensor, values );
+  assert( values.size() == nvalues );
+
+  //Make Tensor
+  TF_Tensor * tensor = TF_AllocateTensor( TF_FLOAT, dims.data(), NDIM, sizeof(float) * nvalues );
+  memcpy( TF_TensorData( tensor ), values.data(), sizeof(float) * nvalues );
+  
 }
 
 } //namespace tfplusplus
